47e31e7408963919211ecac86eccfe8f109ebf90
JulianROUX <julian.roux01@yahoo.fr>
2021-04-21 15:38:25 +0200

    1.1

--- test.sh
+++ test.sh
@@ -2,18 +2,21 @@
 >stonk.txt
 nbsite=$(cat listesites|wc -l)
 while [ "$nbsite" != 0 ]; do
-	
 	lien=$(cat listesites|head -n $nbsite|tail -n 1)
-	wget -O oui "$lien" > oui
+	wget -q -O oui "$lien" > oui
 	((nbsite --))
 	fin= 
 	stonk=init
 	uh=4
-		while [ "$stonk" != "$fin" ]; do
+	while [ "$stonk" != "$fin" ]; do
 		stonk=$(cat oui|grep -i line1=|awk -F '[],[]' '{print $'$uh'}')
 		echo $stonk >> stonk.txt
 		((uh=uh+5))
-		done
-	cat stonk.txt|tail -n 2|head -n 1 >> dprix.txt
+	done
+	prixdolls=$(cat stonk.txt|tail -n 2|head -n 1)
+	echo "$prixdolls $"
+	prixeur=$(echo "scale=2; $prixdolls/1.2"|bc -l)
+	echo "$prixeur €"
+
 done
 rm oui

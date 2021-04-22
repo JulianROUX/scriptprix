15d0164aa7bea6f4f6c42d26cfb54e1d2a3a3542
JulianROUX <julian.roux01@yahoo.fr>
2021-04-22 11:55:43 +0200

    1.2

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
->dprix.txt
+>prixtot.txt
 >stonk.txt
 nbsite=$(cat listesites|wc -l)
+prixtt=0
 while [ "$nbsite" != 0 ]; do
 	lien=$(cat listesites|head -n $nbsite|tail -n 1)
 	wget -q -O oui "$lien" > oui
-	((nbsite --))
+	#((nbsite --))
 	fin= 
 	stonk=init
 	uh=4
@@ -14,9 +15,14 @@ while [ "$nbsite" != 0 ]; do
 		((uh=uh+5))
 	done
 	prixdolls=$(cat stonk.txt|tail -n 2|head -n 1)
+	skin=$(cat listesites|head -n $nbsite |tail -n 1|awk -F '[/]' '{print $7}' |sed "s/%20/ /g" |sed "s/%7C//g"| sed "s/%28/ /g" |sed "s/%29//g"|sed "s/  / /g")
+	echo "Prix de $skin :"
 	echo "$prixdolls $"
 	prixeur=$(echo "scale=2; $prixdolls/1.2"|bc -l)
 	echo "$prixeur €"
-
+	prixtt=$(echo " $prixtt + $prixeur "|bc -l)
+	echo "Le prix total est de $prixtt €"
+	((nbsite--))
 done
 rm oui
+

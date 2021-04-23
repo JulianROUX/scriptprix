f6447e84964b9964e6de83876436aee11b5017f4
JulianROUX <julian.roux01@yahoo.fr>
2021-04-23 16:38:16 +0200

    2.1

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
+echo "calcul cours euro:"
+wget -q -O cours "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml" > cours
+eur=$(cat cours | grep -i USD |awk '{print $3 }' |awk -F '[=]' '{print $2 }'|cut -f2 -d "'")
+echo "1€=$eur$"
 >stonk.txt
 nbsite=$(cat listesites|wc -l)
 prixtt=0
 while [ "$nbsite" != 0 ]; do
 	lien=$(cat listesites|head -n $nbsite|tail -n 1)
 	wget -q -O tmp "$lien" > tmp
-	#((nbsite --))
 	fin= 
 	stonk=init
 	uh=4
@@ -17,7 +20,7 @@ while [ "$nbsite" != 0 ]; do
 	skin=$(cat listesites|head -n $nbsite |tail -n 1|awk -F '[/]' '{print $7}' |sed "s/%20/ /g" |sed "s/%7C//g"| sed "s/%28/ /g" |sed "s/%29//g"|sed "s/  / /g")
 	echo "Prix de $skin :"
 	echo "$prixdolls $"
-	prixeur=$(echo "scale=2; $prixdolls/1.2"|bc -l)
+	prixeur=$(echo "scale=2; $prixdolls/$eur"|bc -l)
 	echo "$prixeur €"
 	prixtt=$(echo " $prixtt + $prixeur "|bc -l)
 	echo "Le prix total est de $prixtt €"
@@ -26,3 +29,4 @@ done
 rm tmp
 temps=$(date +"%d/%m/%Y %H:%M")
 echo " $temps : $prixtt " >> prix.txt
+rm cours

9e86647547dda70e4bc1c0aaa088a08287840aa3
JulianROUX <julian.roux01@yahoo.fr>
2021-04-29 10:06:05 +0200

    2.3

--- calc.sh
+++ calc.sh
@@ -8,13 +8,12 @@ prix=$(cat prix.txt | awk -F '[ ]' '{print $5 }' |tail -n $nbligne|head -n 1)
 moy=$(echo " $moy + $prix "|bc -l)
 moy2=$(echo "scale=2; $moy / $testdiv"|bc -l)
 echo -ne "\r$moy2"
-#echo $testdiv
 ((nbligne--))
 done
 echo
+
 #moy 10 dernieres lignes
-nbligne=10
-# Moyenne total
+nbligne=10;moy=0;testdiv=0
 echo "Moyenne des $nbligne derniers prix:"
 while [ "$nbligne" != 0 ]; do
 prix=$(cat prix.txt | awk -F '[ ]' '{print $5 }' |tail -n $nbligne|head -n 1)
@@ -22,13 +21,12 @@ prix=$(cat prix.txt | awk -F '[ ]' '{print $5 }' |tail -n $nbligne|head -n 1)
 moy=$(echo " $moy + $prix "|bc -l)
 moy2=$(echo "scale=2; $moy / $testdiv"|bc -l)
 echo -ne "\r$moy2"
-#echo $testdiv
 ((nbligne--))
 done
 echo
 
 #min
-pmin=99999
+pmin=99999;bool=0
 nbligne=$(cat prix.txt|wc -l)
 echo "Prix minimum:"
 while [ "$nbligne" != 0 ]; do
@@ -56,3 +54,21 @@ echo -ne "\r$pmax"
 ((nbligne--))
 done
 echo
+
+#Évolution sur les 10 derniers prix
+nbligne=10;testdiv=0;moy=0
+# Moyenne total
+echo "Évolution sur les $nbligne derniers prix:"
+while [ "$nbligne" != 1 ]; do
+prix=$(cat prix.txt | awk -F '[ ]' '{print $5 }' |tail -n $nbligne|head -n 1)
+((testdiv++))
+moy=$(echo " $moy + $prix "|bc -l)
+moy2=$(echo "scale=2; $moy / $testdiv"|bc -l)
+#echo -ne "\r$moy2"
+#echo $testdiv
+((nbligne--))
+done
+dprix=$(cat prix.txt | awk -F '[ ]' '{print $5 }' |tail -n 1)
+evo=$(echo "scale=4;(($dprix/$moy2) * 100) - 100"|bc -l)
+evo=$(echo $evo|sed 's/..$//')
+echo "L'évolution est de $evo %"
